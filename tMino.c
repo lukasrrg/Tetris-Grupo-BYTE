006e7f860a997a5abce474869d4c7bdc3d02bb35
Lucas Varela <lucassvarela@gmail.com>
2026-05-03 11:43:33 -0300

    fix

--- tMino.c
+++ tMino.c
@@ -21,7 +21,7 @@ tGBT_ColorRGB paletaCGA[CANT_COLORES] = {
     {0xFF, 0xFF, 0xFF}  // 15:  Usado como transparente por GBT
 };
 
-void minoCrear(tMino *mino, int pX, int pY, int col)
+void minoCrear(tMino *mino, int pX, int pY, int col) //Funcion para crear tetramino
 {
     mino->color = col;
     mino->posX = pX;
