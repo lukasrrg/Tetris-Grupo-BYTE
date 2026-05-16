49b6df3d816128ead6bd65c60f3dd995905e91a6
Lucas Varela <lucassvarela@gmail.com>
2026-05-16 11:42:38 -0300

    agrego archivos tJugador

---
+++ tJugador.h
@@ -0,0 +1,19 @@
+#ifndef TJUGADOR_H_INCLUDED
+#define TJUGADOR_H_INCLUDED
+
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
+
+#define MAX_NOMBRE 20
+#define ARCHIVO_JUGADORES "jugadores.bin"
+
+typedef struct{
+    char nombre[MAX_NOMBRE];
+    int puntaje_max;
+}tJugador;
+
+bool jugadorExiste(const char* nombre); //Devuelve true si el nombre ya existe en el archivo.
+void jugadorGuardar(const tJugador *jugador); //Guarda un jugador nuevo o sobreeescribe si ya existe.
+
+#endif
